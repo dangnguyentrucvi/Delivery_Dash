ad5bbb25251770216a69d1c458f097e9e774174a
Dang Nguyen Truc Vi <kimnguyen1236@gmail.com>
2026-01-18 22:34:02 +0700

    Using GetComponent

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -7,15 +7,16 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     private bool hasPackage;
-    
+    [SerializeField] private float destroyDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         // if  (the tag is package)
         // then (print "pickeup package to console)
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !hasPackage)
         {
-            Debug.Log("Pickup package");
+            Debug.Log("Picked up package");
             hasPackage = true;
+            Destroy(collision.gameObject, destroyDelay);
         }
 
         else if (collision.CompareTag("Customer") && hasPackage)
